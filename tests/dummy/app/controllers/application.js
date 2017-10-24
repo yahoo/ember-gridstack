4e0b4a2d412f5162c16d06ecdcf4b8210b61add7
Jon Kilroy <kilroy@yahoo-inc.com>
2017-10-24 09:10:57 -0500

    Refactor dummy app to add new items to the bottom

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
 
+const { get } = Ember;
+
 export default Ember.Controller.extend({
-  items: Ember.A([1, 2, 3]),
+  items: Ember.A([0, 1, 2]),
 
   actions: {
     changeItems() {
-      this.get('items').pushObject(4);
+      let items = get(this, 'items');
+      items.pushObject(get(items, 'length') + 1);
     }
   }
 });
