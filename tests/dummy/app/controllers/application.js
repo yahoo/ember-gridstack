70b67272d2a96397b3d65f5026fef35fde405e0a
Balaji Manoharan <balaji.mano3@gmail.com>
2020-02-17 21:20:40 -0600

    Change counter

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,9 +1,18 @@
 import { A } from '@ember/array';
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class extends Controller {
   items = A([0, 1, 2])
+
+  @tracked
+  num = 0
+
+  @action
+  onChange() {
+    this.num = this.num + 1;
+  }
 
   @action
   changeItems() {
