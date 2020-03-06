a07b9529d8cab8b9461859903873ff4c5c04da96
Till Theato <till.theato@evocount.de>
2020-03-06 10:24:32 +0100

    Fix conflicts with other jquery addons.
    See https://github.com/gridstack/gridstack.js/issues/1191

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    app.import('node_modules/gridstack/dist/gridstack.all.js');
+
+    app.import('node_modules/gridstack/dist/jquery-ui.js');
+    app.import('node_modules/gridstack/dist/gridstack.js');
+    app.import('node_modules/gridstack/dist/gridstack.jQueryUI.js');
 
     app.import('node_modules/gridstack/dist/gridstack.css');
     app.import('node_modules/gridstack/dist/gridstack-extra.css');
