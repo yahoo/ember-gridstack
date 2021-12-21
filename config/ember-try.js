6a84cff297cb237370f3fff62a2f0344e3735e33
Jon Kilroy <kilroy@verizonmedia.com>
2021-12-21 15:46:32 -0600

    ci: update ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -26,27 +26,39 @@ module.exports = async function () {
       {
         name: 'ember-release',
         npm: {
+          dependencies: {
+            'ember-auto-import': '^2.0.0',
+          },
           devDependencies: {
             'ember-source': await getChannelURL('release'),
+            webpack: '^5.0.0',
           },
         },
       },
-      // {
-      //   name: 'ember-beta',
-      //   npm: {
-      //     devDependencies: {
-      //       'ember-source': await getChannelURL('beta'),
-      //     },
-      //   },
-      // },
-      // {
-      //   name: 'ember-canary',
-      //   npm: {
-      //     devDependencies: {
-      //       'ember-source': await getChannelURL('canary'),
-      //     },
-      //   },
-      // },
+      {
+        name: 'ember-beta',
+        npm: {
+          dependencies: {
+            'ember-auto-import': '^2.0.0',
+          },
+          devDependencies: {
+            'ember-source': await getChannelURL('beta'),
+            webpack: '^5.0.0',
+          },
+        },
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          dependencies: {
+            'ember-auto-import': '^2.0.0',
+          },
+          devDependencies: {
+            'ember-source': await getChannelURL('canary'),
+            webpack: '^5.0.0',
+          },
+        },
+      },
       {
         name: 'ember-default-with-jquery',
         env: {
